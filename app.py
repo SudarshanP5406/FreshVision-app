111d00a940219985aa30e9df7050ee1fa9b5fe13
Sudarshan P <sudarshan4506@gmail.com>
2025-05-04 22:15:14 +0530

    Fix port binding for deployment

--- app.py
+++ app.py
@@ -46,6 +46,5 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
